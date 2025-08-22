9ed2b93827af5f5c9b182d76f7cedd51ab6f1a35
unknown <amandarccamargo@gmail.com>
2025-08-21 22:11:31 -0300

    Projeto Criado

---
+++ src/main/java/com/mandy/festa/PessoaModel.java
@@ -0,0 +1,49 @@
+package com.mandy.festa;
+
+import jakarta.persistence.*;
+
+@Table (name = "PESSOA")
+@Entity (name = "TBL_PESSOA")
+
+
+
+public class PessoaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private int idade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
